45c3d1a6f7d7603653f0bb12c207a2990d622c94
Peter Lichard <peter.lichard@gmail.com>
2018-01-31 13:11:44 +0100

    added text info and command help, can draw stuff, clear, amazing!

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 
 //using namespace std;
 
+enum DRAW_TYPE {
+    DRAW_POINT = 1,
+    DRAW_RANDOM = 2,
+    DRAW_CLEAR = 3,
+    DRAW__COUNT
+};
+
 namespace {
     const bool USE_GL45 = false; // do not use yet, doesn't work!
 
@@ -31,10 +38,38 @@ namespace {
 
     bool do_spawn = false;
     sf::Vector2f spawn_pos;
+    DRAW_TYPE draw_type = DRAW_RANDOM;
 
     bool left_down = false;
 
+    bool do_update = false;
+    bool run_update = false;
+
     sf::Clock sfclock;
+
+    sf::Font font;
+    sf::Text text;
+    sf::RectangleShape text_bg;
+}
+
+void init_world() {
+    sf::Uint8* data = new sf::Uint8[GAME_W*GAME_H*4];
+    for(int i = 0; i < GAME_W*GAME_H; i++) {
+        //const sf::Uint8 c = 255*(dis(gen) > 128);
+        data[i*4 + 0] = 0;
+        data[i*4 + 1] = 0;
+        data[i*4 + 2] = 0;
+        data[i*4 + 3] = 255;
+    }
+
+    init_tex.update(data);
+    delete [] data;
+
+    sprite.setTexture(init_tex);
+
+    tex1->draw(sprite);
+
+    run_update = false;
 }
 
 void init() {
@@ -50,19 +85,7 @@ void init() {
         // (the "floating" title is used for tiling window managers to "float" the window)
         win.create(sf::VideoMode(800,800), "floating");
     }
-
-    sf::Uint8* data = new sf::Uint8[GAME_W*GAME_H*4];
-    for(int i = 0; i < GAME_W*GAME_H; i++) {
-        const sf::Uint8 c = 255*(dis(gen) > 128);
-        data[i*4 + 0] = 0;
-        data[i*4 + 1] = 0;
-        data[i*4 + 2] = c;
-        data[i*4 + 3] = 255;
-    }
-
     init_tex.create(GAME_W, GAME_H);
-    init_tex.update(data);
-    delete [] data;
 
     tex1 = new sf::RenderTexture();
     tex2 = new sf::RenderTexture();
@@ -78,9 +101,7 @@ void init() {
     tex1->setSmooth(false);
     tex2->setSmooth(false);
 
-    sprite.setTexture(init_tex);
-
-    tex1->draw(sprite);
+    init_world();
 
     render_view = update_view;
     win.setView(render_view);
@@ -97,13 +118,21 @@ void init() {
         render_shader.loadFromFile("update.vert", "render.frag");
         spawn_shader.loadFromFile("update.vert", "spawn.frag");
     }
+
+
+    font.loadFromFile("fonts/Inconsolata-Regular.ttf");
+    text.setFont(font);
+    text.setCharacterSize(14);
+
+
+    text_bg.setSize({100, text.getCharacterSize()*(DRAW__COUNT + 3)});
+    text_bg.setPosition(0,0);
+    text_bg.setFillColor(sf::Color(128,128,128, 240));
 }
 
 void run() {
     sf::Event ev;
     while(win.isOpen()) {
-
-        bool do_update = true;
 
         while(win.pollEvent(ev)) {
             switch (ev.type) {
@@ -111,11 +140,30 @@ void run() {
                 win.close();
                 break;
             case sf::Event::KeyPressed:
-                if(ev.key.code == sf::Keyboard::N) {
+                switch(ev.key.code) {
+                case sf::Keyboard::N:
                     do_update = true;
-                }
-                else if(ev.key.code == sf::Keyboard::Q) {
+                    break;
+                case sf::Keyboard::Q:
                     win.close();
+                    break;
+                case sf::Keyboard::Space:
+                    run_update = !run_update;
+                    break;
+                case sf::Keyboard::R:
+                    init_world();
+                    break;
+                case sf::Keyboard::Num1:
+                    draw_type = DRAW_POINT;
+                    break;
+                case sf::Keyboard::Num2:
+                    draw_type = DRAW_RANDOM;
+                    break;
+                case sf::Keyboard::Num3:
+                    draw_type = DRAW_CLEAR;
+                    break;
+                default:
+                    break;
                 }
                 break;
             case sf::Event::MouseWheelScrolled:
@@ -148,12 +196,11 @@ void run() {
             case sf::Event::MouseMoved:
                 {
                     sf::Vector2i mouse_pos = {ev.mouseMove.x, ev.mouseMove.y};
-                    sf::Vector2f delta = win.mapPixelToCoords(mouse_pos) - win.mapPixelToCoords(mouse_prev_pos);
+                    sf::Vector2f delta = win.mapPixelToCoords(mouse_pos, render_view) - win.mapPixelToCoords(mouse_prev_pos, render_view);
                     mouse_prev_pos = mouse_pos;
-                    spawn_pos = win.mapPixelToCoords(mouse_pos);
+                    spawn_pos = win.mapPixelToCoords(mouse_pos, render_view);
                     if (left_down) {
                         render_view.move(-delta.x, -delta.y);
-                        win.setView(render_view);
                     }
                 }
                 break;
@@ -166,6 +213,7 @@ void run() {
         }
 
         if (do_spawn) {
+            spawn_shader.setUniform("type", (int)draw_type);
             spawn_shader.setUniform("spawn", spawn_pos);
             spawn_shader.setUniform("T", sfclock.getElapsedTime().asSeconds());
 
@@ -174,19 +222,59 @@ void run() {
             std::swap(tex1, tex2);
         }
 
-        if (do_update) {
+        if (run_update || do_update) {
             update_shader.setUniform("GAME_W", (float)GAME_W);
             update_shader.setUniform("GAME_H", (float)GAME_H);
 
             sprite.setTexture(tex1->getTexture());
             tex2->draw(sprite, &update_shader);
             std::swap(tex1, tex2);
+            do_update = false;
         }
 
         sprite.setTexture(tex1->getTexture());
         win.setView(render_view);
         win.clear();
         win.draw(sprite, &render_shader);
+
+
+        win.setView(win.getDefaultView());
+        win.draw(text_bg);
+        if(draw_type == DRAW_POINT) text.setFillColor(sf::Color::Green); else text.setFillColor(sf::Color::White);
+        text.setPosition(5,0);
+        text.setString("1   : point");
+        win.draw(text);
+
+        if(draw_type == DRAW_RANDOM) text.setFillColor(sf::Color::Green); else text.setFillColor(sf::Color::White);
+        text.move(0,text.getCharacterSize());
+        text.setString("2   : random");
+        win.draw(text);
+
+        if(draw_type == DRAW_CLEAR) text.setFillColor(sf::Color::Green); else text.setFillColor(sf::Color::White);
+        text.move(0,text.getCharacterSize());
+        text.setString("3   : clear");
+        win.draw(text);
+
+        if(run_update) {
+             text.setFillColor(sf::Color::Green);
+             text.setString("spc : running");
+        }
+        else {
+            text.setFillColor(sf::Color::Red);
+            text.setString("spc : stopped");
+        }
+        text.move(0,text.getCharacterSize());
+        win.draw(text);
+
+        text.setFillColor(sf::Color::White);
+        text.move(0,text.getCharacterSize());
+        text.setString("R   : reset");
+        win.draw(text);
+
+        text.setFillColor(sf::Color::White);
+        text.move(0,text.getCharacterSize());
+        text.setString("N   : step");
+        win.draw(text);
 
         win.display();
     }
--- spawn.frag
+++ spawn.frag
@@ -4,6 +4,8 @@ in vec2 pos;
 uniform vec2 spawn;
 uniform float T;
 
+uniform int type;
+
 float rand(vec2 co){
     return fract(T*sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
 }
@@ -11,15 +13,35 @@ float rand(vec2 co){
 void main()
 {
     vec4 pixel = texture2D(texture, gl_TexCoord[0].xy);
-
-    if ( distance(pos, spawn) < 20 ) {
-        if (rand(pos) > 0.5) {
+    // DRAW_POINT
+    if (type == 1) {
+        if (abs(pos.x - spawn.x) < 0.4 && abs(pos.y - spawn.y) < 0.4) {
             gl_FragColor = vec4(pixel.rg, 1, 1);
-        } else {
-            gl_FragColor = vec4(pixel.rg, 0, 1);
+        }
+        else {
+            gl_FragColor = pixel;
         }
     }
-    else {
-        gl_FragColor = pixel;
+    // DRAW_RANDOM
+    else if (type == 2) {
+        if ( distance(pos, spawn) < 20 ) {
+            if (rand(pos) > 0.5) {
+                gl_FragColor = vec4(pixel.rg, 1, 1);
+            } else {
+                gl_FragColor = vec4(pixel.rg, 0, 1);
+            }
+        }
+        else {
+            gl_FragColor = pixel;
+        }
+    }
+    // DRAW_CLEAR
+    else if (type == 3) {
+        if ( distance(pos, spawn) < 20 ) {
+            gl_FragColor = vec4(pixel.rg, 0, 1);
+        }
+        else {
+            gl_FragColor = pixel;
+        }
     }
 }
